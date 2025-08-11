059121859a5d84dda580fba0ebfa4f0fc702dd53
Woobeen Jeon <woobeen0516@gmail.com>
2025-08-11 10:23:34 +0900

    Null 확인 추가

--- api/src/main/java/com/bindglam/bm4cutscene/cutscene/CutsceneProperties.java
+++ api/src/main/java/com/bindglam/bm4cutscene/cutscene/CutsceneProperties.java
@@ -1,13 +1,15 @@
 package com.bindglam.bm4cutscene.cutscene;
 
+import java.util.Objects;
+
 public record CutsceneProperties(String modelId, String animation, boolean shiftToClose) {
     public static Builder builder() {
         return new Builder();
     }
 
     public static class Builder {
-        private String modelId = "";
-        private String animation = "";
+        private String modelId;
+        private String animation;
         private boolean shiftToClose = true;
 
         private Builder() {
@@ -29,6 +31,9 @@ public record CutsceneProperties(String modelId, String animation, boolean shift
         }
 
         public CutsceneProperties build() {
+            Objects.requireNonNull(modelId);
+            Objects.requireNonNull(animation);
+
             return new CutsceneProperties(modelId, animation, shiftToClose);
         }
     }
